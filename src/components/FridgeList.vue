2b70035e8b33c57c4d35369514a89c218e46dc24
Léa Doublet <leadbtcode@gmail.com>
2024-05-28 10:00:27 +0200

    add product filter

--- src/components/FridgeList.vue
+++ src/components/FridgeList.vue
@@ -1,29 +1,49 @@
 <template>
-    <v-list>
-        <v-list-item v-for="product in products" :key="product.id">
-            <v-list-item-content>
-                <v-list-item-title>{{ product.nom }}</v-list-item-title>
-                <v-list-item-subtitle>Quantité: {{ product.qte }}</v-list-item-subtitle>
-            </v-list-item-content>
-        </v-list-item>
-    </v-list>
+    <div>
+        <h2>Liste des produits dans le frigo :</h2>
+        <v-text-field v-model="searchTerm" label="Rechercher des produits" outlined @input="filterProducts" />
+        <v-list>
+            <v-list-item v-for="product in filteredProducts" :key="product.id">
+                <v-list-item-content>
+                    <v-list-item-title>{{ product.nom }}</v-list-item-title>
+                    <v-list-item-subtitle>Quantité: {{ product.qte }}</v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+        </v-list>
+    </div>
 </template>
 
 <script setup>
 
 import { ref, onMounted } from 'vue';
 const products = ref([]);
+const filteredProducts = ref([]);
+const searchTerm = ref('');
 
 onMounted(async () => {
     try {
+        //On recupère les produits de l'api avec le bon id etudiant
         const response = await fetch('https://webmmi.iut-tlse3.fr/~pecatte/frigo/public/4/produits');
         if (!response.ok) {
             throw new Error('Erreur lors de la récupération des données');
         }
         const data = await response.json();
         products.value = data;
+        //console.log(products.value)
+        filteredProducts.value = data; // Initialisation de la liste qui va contenir les produits filtrés
     } catch (error) {
         console.error('Erreur :', error.message);
     }
 });
+
+const filterProducts = () => {
+    if (!searchTerm.value) {
+        // Si le champ de recherche est vide, on affiche tous les produits
+        filteredProducts.value = products.value;
+    } else {
+        // Sinon on filtre les produits en fonction du terme de recherche
+        filteredProducts.value = products.value.filter(product => product.nom.toLowerCase().includes(searchTerm.value.toLowerCase()));
+    }
+}
+
 </script>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <v-text-field v-model="searchTerm" label="Rechercher des produits" @input="searchProducts" />
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const searchTerm = ref('');
+const searchProducts = () => {
+    // On créer un événement personnalisé avec emit -->https://vuejs.org/guide/components/events.html
+    emit('search', searchTerm.value);
+}
+</script>
